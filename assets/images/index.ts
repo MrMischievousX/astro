465f07be7df9230ad0a240d9e25f0523799f0521
akash tureha <akashtureha@akashs-MacBook-Air.local>
2025-09-01 19:51:00 +0530

    adding assets and fonts

---
+++ assets/images/index.ts
@@ -0,0 +1,5 @@
+const GradientBg = require("./gradient-bg.png");
+const GradientBg1 = require("./gradient-bg-1.png");
+const GradientBg2 = require("./gradient-bg-2.png");
+
+export { GradientBg, GradientBg1, GradientBg2 };
---
+++ assets/svgs/index.ts
@@ -0,0 +1,7 @@
+import BackSvg from "./back.svg";
+import HistorySvg from "./history.svg";
+import LanguageSvg from "./language.svg";
+import LineSvg from "./line.svg";
+import ZodiacContainerSvg from "./zodiac-container.svg";
+
+export { BackSvg, HistorySvg, LanguageSvg, LineSvg, ZodiacContainerSvg };
